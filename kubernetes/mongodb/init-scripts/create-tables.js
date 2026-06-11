8d1088164a45a30255fb969735af0157c620b101
evgeny-kor-m <evgeny.korchev@gmail.com>
2026-06-11 15:35:13 +0300

    Create create-tables.js

---
+++ kubernetes/mongodb/init-scripts/create-tables.js
@@ -0,0 +1,27 @@
+db.createCollection("reservations", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["reservation_id", "full_name", "email_address", 
+                       "checkIn_date", "checkOut_date", "hotel_id"],
+            properties: {
+                reservation_id: {
+                    bsonType: "int",
+                    description: "must, not null"
+                },
+                full_name: {
+                    bsonType: "string",
+                    description: "must, not null"
+                },
+                email_address: {
+                    bsonType: "string",
+                    description: "must, not null"
+                },
+                hotel_id: {
+                    bsonType: "int",
+                    description: "must, not null"
+                }
+            }
+        }
+    }
+})
