9908d49eba7f23d333c168cb59fff05061540af7
evgeny-kor-m <evgeny.korchev@gmail.com>
2026-06-11 15:42:09 +0300

    Update create-tables.js

--- kubernetes/mongodb/init-scripts/create-tables.js
+++ kubernetes/mongodb/init-scripts/create-tables.js
@@ -2,8 +2,7 @@ db.createCollection("reservations", {
     validator: {
         $jsonSchema: {
             bsonType: "object",
-            required: ["reservation_id", "full_name", "email_address", 
-                       "checkIn_date", "checkOut_date", "hotel_id"],
+            required: ["reservation_id", "full_name", "email_address", "checkIn_date", "checkOut_date", "hotel_id"],
             properties: {
                 reservation_id: {
                     bsonType: "int",
@@ -26,6 +25,36 @@ db.createCollection("reservations", {
                     description: "must, not null"
                 },
                 hotel_id: {
+                    bsonType: "int",
+                    description: "must, not null"
+                }
+            }
+        }
+    }
+})
+db.createCollection("hotels", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["hotel_id", "hotel_name", "location", "price_per_night"],
+            properties: {
+                hotel_id: {
+                    bsonType: "int",
+                    description: "must, not null"
+                },
+                hotel_name: {
+                    bsonType: "string",
+                    description: "must, not null"
+                },
+                description: {
+                    bsonType: "string",
+                    description: "optional"
+                },
+                location: {
+                    bsonType: "string",
+                    description: "must, not null"
+                },
+                price_per_night: {
                     bsonType: "int",
                     description: "must, not null"
                 }
